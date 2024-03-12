c3047f83532825a232e2968f6086d1f16a918e9c
Pamela <143747791+PamelaChB@users.noreply.github.com>
2024-03-12 14:08:22 -0600

    Primer commit

---
+++ server/src/main/java/com/example/reproductormusica/MainSceneController.java
@@ -0,0 +1,37 @@
+package com.example.reproductormusica;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainSceneController implements Initializable {
+
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+    }
+
+    public void playMedia() {
+
+    }
+    public void stopMedia() {
+
+    }
+    public void previousMedia() {
+
+    }
+    public void nextMedia() {
+
+    }
+    public void beginTimer() {
+
+    }
+    public void cancelTimer() {
+
+    }
+
+
+
+}
