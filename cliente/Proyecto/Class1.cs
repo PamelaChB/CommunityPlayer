e3b232965b107118effac268312a64763d7a65bf
FabiMC040105 <149991170+FabiMC040105@users.noreply.github.com>
2024-04-12 14:50:49 -0600

    para mezcla

---
+++ cliente/Proyecto/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Proyecto
+{
+    internal class Mensaje
+    {
+        [JsonProperty("command")]
+        public string Command { get; set; }
+
+        public Mensaje(string command)
+        {
+            Command = command;
+
+
+        }
+        public string ToJson()
+        { 
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
